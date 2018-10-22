bccb396a32e706313886e322507a1e967a2c45e1
nadienkovic <43369715+nadienkovic@users.noreply.github.com>
2018-10-22 22:30:07 +0200

    Update conditional.h

--- conditional.h
+++ conditional.h
@@ -1,6 +1,6 @@
 #include "stdio.h"
 
-int* number;
-int* choice;
-void GetNumber(int* number);
-void AddOrMult(int* choice);
+int setOperator();
+int getNumber();
+void Add(int* number);
+void Mult(int* number)
